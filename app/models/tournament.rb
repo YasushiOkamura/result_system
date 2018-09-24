600968a15c795c75e33db22a41bbc305b2e47e12
YasushiOkamura <okamura@net.lab.uec.ac.jp>
2018-09-25 03:12:44 +0900

    フォームのバリデーション

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,4 +3,6 @@ class Tournament < ApplicationRecord
   has_many :long_results
   has_many :field_results
   has_many :relay_results
+
+  validates :name, :place, :start_day, :end_day, presence: true
 end

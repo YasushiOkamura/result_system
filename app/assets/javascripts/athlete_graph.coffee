9954d8eafab87e0301c388473c95425f1afbad74
YasushiOkamura <okamura@net.lab.uec.ac.jp>
2018-10-26 20:23:51 +0900

    グラフに表示される情報を修正した

--- app/assets/javascripts/athlete_graph.coffee
+++ app/assets/javascripts/athlete_graph.coffee
@@ -1,15 +1,3 @@
-color = (context) ->
-  index = context.dataIndex
-  value = context.dataset.data[index]
-  return value == Nan ? 'red' : 'blue'
-
-label_to_date = ->
-  labels = []
-  for label in gon.label
-    labels.push(new Date(label))
-  return labels
-
-
 window.draw_graph = ->
   ctx = document.getElementById("athleteChart").getContext('2d')
   data = {
@@ -19,12 +7,20 @@ window.draw_graph = ->
       data: gon.results,
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderColor: 'rgba(75, 192, 192, 1)',
+      pointBorderColor: gon.colors,
       borderWidth: 2
     }]
   }
   options = {
+    showAllTooltips: true,
     tooltips: {
-      mode: 'nearest'
+      mode: 'nearest',
+      callbacks: {
+        title: (tooltipItem, data) ->
+          return [tooltipItem[0].xLabel, gon.tournaments[tooltipItem[0].index]]
+        label: (tooltipItem, data) ->
+          return gon.winds[tooltipItem.index] + tooltipItem.yLabel
+      }
     },
     scales: {
       yAxes: [{
--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,7 +1,9 @@
 class AthletesController < ApplicationController
   before_action :set_athlete, only: [:show, :graph]
   before_action :set_pb, only: [:show]
-  
+  BLUE = 'rgba(75, 192, 192, 1)'
+  RED = 'rgba(255, 100, 100, 1)'
+
   def index
     @athletes = Athlete.order('active desc, grade asc, id desc').page(params[:page]).per(30)
   end
@@ -10,11 +12,7 @@ class AthletesController < ApplicationController
   end
 
   def graph
-    redirect_to athlete_path(@athlete) if set_result.nil? || @results.size.zero?
-    gon.dates = @dates
-    gon.results = @results
-    gon.max = @max
-    gon.min = @min
+    redirect_to athlete_path(@athlete) if set_result.nil? || gon.results.size.zero?
   end
 
   private
@@ -36,21 +34,30 @@ class AthletesController < ApplicationController
     return nil unless @competition
     case @type
     when 'short'
-      results = ShortResult.where(athlete_id: @athlete.id, competition: @competition, official: true).where.not(result: nil).order('established_date asc')
+      results = ShortResult.where(athlete_id: @athlete.id, competition: @competition).where.not(result: nil).order('established_date asc')
     when 'long'
-      results = LongResult.where(athlete_id: @athlete.id, competition: @competition, official: true).where.not(result: nil).order('established_date asc')
+      results = LongResult.where(athlete_id: @athlete.id, competition: @competition).where.not(result: nil).order('established_date asc')
     when 'field'
-      results = FieldResult.where(athlete_id: @athlete.id, competition: @competition, official: true).where.not(result: nil).order('established_date asc')
+      results = FieldResult.where(athlete_id: @athlete.id, competition: @competition).where.not(result: nil).order('established_date asc')
     else
       return nil
     end
-
-    @min = ['short', 'long'].include?(@type) ? results.minimum(:result).to_f/ 1000 : results.minimum(:result)
-    @max = ['short', 'long'].include?(@type) ? results.maximum(:result).to_f/ 1000 : results.maximum(:result)
+    
     @years = results.pluck(:established_date).map(&:year).uniq
+    gon.min = ['short', 'long'].include?(@type) ? results.minimum(:result).to_f/ 1000 : results.minimum(:result)
+    gon.max = ['short', 'long'].include?(@type) ? results.maximum(:result).to_f/ 1000 : results.maximum(:result)
     results = results.where(established_date: Date.new(params[:year].to_i)...Date.new(params[:year].to_i, 12, 31)) if params[:year].present?
+    
+    result_data = results.pluck(:official, :tournament_id, :established_date, :result, :wind) if ['short', 'field'].include?(@type)
+    result_data = results.pluck(:official, :tournament_id, :established_date, :result) if ['long'].include?(@type)
+    gon.colors = result_data.map { |r| r[0] ? BLUE : RED }
+    gon.tournaments = []
+    results.each do |result|
+      gon.tournaments << result.tournament.name
+    end
 
-    @dates = results.pluck(:established_date).map(&:to_s)
-    @results = results.pluck(:result).map { |r| ['short', 'long'].include?(params[:type]) ? (r.to_f / 1000).to_s : r.to_s }
+    gon.dates = result_data.map { |r| r[2].to_s }
+    gon.results = result_data.map { |r| ['short', 'long'].include?(@type) ? (r[3].to_f / 1000).to_s : r[3].to_s }
+    gon.winds = ['short', 'field'].include?(@type) ? result_data.map { |r| r[4].present? ? "(#{r[4] >= 0.0 ? '+' : ''}#{r[4]})" : "" } :  Array.new(results.size, "")
   end
 end

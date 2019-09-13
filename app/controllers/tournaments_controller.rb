90246e12946910471bee5dac51dc954f6ca66902
yacchiDAO <okamura@net.lab.uec.ac.jp>
2019-09-13 15:53:57 +0900

    大会一覧ページのソートを開催日時順に変更

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,7 +2,7 @@ class TournamentsController < ApplicationController
   before_action :set_tournament, only: [:show]
   
   def index
-    @tournaments = Tournament.order('id desc').page(params[:page])
+    @tournaments = Tournament.order('start_day desc').page(params[:page])
   end
 
   def show

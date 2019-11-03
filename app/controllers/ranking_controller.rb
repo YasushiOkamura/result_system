d8b50cba0499f0bc5aa99fc671a411b3bdcb5881
yacchiDAO <okamura@net.lab.uec.ac.jp>
2019-11-03 15:15:49 +0900

    クエリ節約

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -22,16 +22,20 @@ class RankingController < ApplicationController
     Competition.where(kind: @kind).each do |competition|
       case @kind
       when 'field'
-        @all_results << FieldResult.joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.max_by(&:result) }.sort_by(&:result).reverse.take(5)
+        @all_results << FieldResult.includes(:athlete, :tournament).joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.max_by(&:result) }.sort_by(&:result).reverse.take(5)
       when 'short'
-        @all_results << ShortResult.joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.min_by(&:result) }.sort_by(&:result).take(5)
+        @all_results << ShortResult.includes(:athlete, :tournament).joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.min_by(&:result) }.sort_by(&:result).take(5)
       when 'long'
-        @all_results << LongResult.joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.min_by(&:result) }.sort_by(&:result).take(5)
+        @all_results << LongResult.includes(:athlete, :tournament).joins(:athlete).where(competition: competition.name, official: true).where.not(result: nil).group_by(&:athlete_id).map { |_id, s| s.min_by(&:result) }.sort_by(&:result).take(5)
       when 'relay'
-        @all_results << RelayResult.where(competition: competition.name, official: true).where.not(result: nil).order('result').limit(10)
+        @all_results << RelayResult.includes(:tournament, :first_athlete, :second_athlete, :third_athlete, :fourth_athlete).where(competition: competition.name, official: true).where.not(result: nil).order('result').limit(10)
       when 'decathlon'
-        @all_results << DecathlonResult.joins(:athlete).where(official: true).where.not(total_score: nil).group_by(&:athlete_id).map { |_id, s| s.max_by(&:total_score) }.sort_by(&:total_score).reverse.take(5)
+        @all_results << DecathlonResult.includes(decathlon_includes_list).joins(:athlete).where(official: true).where.not(total_score: nil).group_by(&:athlete_id).map { |_id, s| s.max_by(&:total_score) }.sort_by(&:total_score).reverse.take(5)
       end
     end
+  end
+
+  def decathlon_includes_list
+    [:athlete, :tournament, :short_100m_result, :field_lj_result, :field_sp_result, :field_hj_result, :short_400m_result, :short_110mh_result, :field_dt_result, :field_pj_result, :field_jt_result, :long_1500m_result]
   end
 end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,7 +7,13 @@ class TournamentsController < ApplicationController
     @tournaments = Tournament.order('start_day desc').page(params[:page])
   end
 
-  def show; end
+  def show
+    @short_results = @tournament.short_results.includes(:athlete).order('competition desc, result asc')
+    @long_results = @tournament.long_results.includes(:athlete).order('competition desc, result asc')
+    @field_results = @tournament.field_results.includes(:athlete).order('competition desc, result desc')
+    @relay_results = @tournament.relay_results.order('competition desc, result asc')
+    @decathlon_results = @tournament.decathlon_results.includes(:athlete).order('total_score desc')
+  end
 
   private
 
